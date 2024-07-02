8ce87bde523a0d5c577b8ba8f607ad662a5f64ed
Manuel <manugonzalez.tech@gmail.com>
2024-07-02 10:13:08 +0100

    Add query strings demo

---
+++ demos/week07/react-query-strings/src/lib/testimonialsData.js
@@ -0,0 +1,7 @@
+export const testimonials = [
+  "Bourbon Review",
+  "Custard Cream Review",
+  "Ginger Nuts Review",
+  "Hobnobs Review",
+  "Chocolate Cookies",
+];
--- demos/week07/react-query-strings/src/pages/UserTestimonials.jsx
+++ demos/week07/react-query-strings/src/pages/UserTestimonials.jsx
@@ -1,7 +1,36 @@
+//import testimonials from library to use as test data
+import { testimonials } from "../lib/testimonialsData";
+
+//import useSearchParams hook to declare query strings
+import { useSearchParams } from "react-router-dom";
 export default function UserTestimonials() {
+  //we are destructuring a variable from an array to store the value of my searchParams
+  const [searchParams] = useSearchParams();
+  //here we are declaring the word that we want to use in the url query string, in this case, sort
+  const sort = searchParams.get("sort");
+
+  //logic to organise array itemsin ascending or descending order
+  if (sort === "asc") {
+    testimonials.sort();
+  } else if (sort === "desc") {
+    testimonials.sort().reverse();
+  }
+
+  //TODO you could add Links so the user can sort the testimonials without typing in the url
+  //TODO you need Link, useParams
+
   return (
     <>
       <h1>User Testimonials</h1>
+      <p>sort: {sort}</p>
+      {testimonials.map((testimonial, index) => {
+        return <p key={index}>{testimonial}</p>;
+      })}
     </>
   );
 }
+
+//map without the return keyword (implicit)
+// {testimonials.map((testimonial) => (
+//   <p key={testimonial}>{testimonial}</p>
+// ))}
